b62e59cef07f9fffc0de99dabc5029d5cbd888b0
Javier <javier.merida@mykukun.com>
2021-10-30 11:30:09 +0200

    Updating Lightweight class

--- src/main/lightweight/a/ProcessLWA1.java
+++ src/main/lightweight/a/ProcessLWA1.java
@@ -3,8 +3,8 @@ package main.lightweight.a;
 import process.Lightweight;
 
 public class ProcessLWA1 extends Lightweight {
-    public ProcessLWA1(String myID) {
-        super(myID);
+    public ProcessLWA1(String myID, int serverPort) {
+        super(myID, serverPort);
     }
 
     public static void main(String[] args) {
--- src/process/Lightweight.java
+++ src/process/Lightweight.java
@@ -1,4 +1,59 @@
 package process;
 
+import communication.Chatter;
+
 public class Lightweight {
+    private final String myID;
+    private volatile int token;
+    private final Chatter server;
+
+    public Lightweight(String myID, int serverPort) {
+        this.myID = myID;
+        this.server = new Chatter();
+    }
+
+    private void execute() {
+    /* TODO: Review and implement
+    while(1) {
+        waitHeavyWeight();
+        requestCS();
+        for (int i = 0; i < 10; i++) {
+            printf("I am the process lightweight %s\n", myID);
+            espera1Segon();
+        }
+        releaseCS();
+        notifyHeavyWeight();
+    }
+     */
+    }
+
+    public void printMsg() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("I am the process lightweight" + myID);
+            wait(1000);
+        }
+    }
+
+    private static void wait(int ms) {
+        try {
+            Thread.sleep(ms);
+        }
+        catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public void waitHeavyWeight() {
+        while (token == -1)
+            ;
+    }
+
+    public int getToken() {
+        return token;
+    }
+
+    public void setToken(int token) {
+        this.token = token;
+    }
+
 }
